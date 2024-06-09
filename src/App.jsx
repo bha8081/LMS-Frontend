67d4668a7415bdf96ce16cdff531a83920e23401
bha8081 <bhaveshy2000r@gmail.com>
2024-06-09 10:24:33 +0530

    Add the Course route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { Route, Routes } from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs';
 import HomePage from './Pages/HomePage';
+import NotFound from './Pages/NotFound';
+import SignUp from './Pages/SingUp';
+import Login from './Pages/Login';
+import CourseList from './Pages/Course/CourseList';
 
 function App() {
 
@@ -12,6 +16,13 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} > </Route>
         <Route path='/about' element={<AboutUs />} > </Route>
+
+        <Route path='/courses' element={<CourseList />} > </Route>
+
+        <Route path='/signup' element={<SignUp />} > </Route>
+        <Route path='/login' element={<Login />} > </Route>
+
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </>
   )

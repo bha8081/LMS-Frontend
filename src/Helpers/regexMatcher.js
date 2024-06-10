5e349da03f643519ba70a5aed85e46fb8bf5c6cf
bha8081 <bhaveshy2000r@gmail.com>
2024-06-10 08:59:56 +0530

    Match the emai and password validation

---
+++ src/Helpers/regexMatcher.js
@@ -0,0 +1,7 @@
+export function isEmail(string) {
+    return string.match(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+}
+
+export function isValidPassword(string) {
+    return string.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/);
+}

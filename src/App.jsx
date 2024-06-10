d1752374c7a81b93df10a11dab4acdbacd73352a
bha8081 <bhaveshy2000r@gmail.com>
2024-06-10 16:49:50 +0530

    Add the only admin create course route

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import CourseList from './Pages/Course/CourseList';
 import Contact from './Pages/Contact';
 import Denied from './Pages/Denied';
 import CourseDescription from './Pages/Course/CourseDescription';
+import RequierAuth from './Components/Auth/RequireAuth';
+import CreateCourse from './Pages/Course/CreateCourse';
 
 function App() {
 
@@ -27,6 +29,10 @@ function App() {
 
         <Route path='/signup' element={<SignUp />} > </Route>
         <Route path='/login' element={<Login />} > </Route>
+
+        <Route element={<RequierAuth allowedRoles={["ADMIN"]} />}>
+        <Route path='/course/create' element={<CreateCourse />} > </Route>
+        </Route>
 
         <Route path='*' element={<NotFound />}></Route>
       </Routes>

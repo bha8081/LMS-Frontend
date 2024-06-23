82da993a1c10df715f93662f5206f73c781ff405
bha8081 <bhaveshy2000r@gmail.com>
2024-06-23 15:35:54 +0530

    Add Checkout failer page and payment cancelation flow

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import Profile from './Pages/User/Profile';
 import EditProfile from './Pages/User/EditProfile';
 import Checkout from './Pages/Payment/Checkout';
 import CheckoutSuccess from './Pages/Payment/CheckoutSuccess';
+import CheckoutFailure from './Pages/Payment/CheckoutFailure';
 
 function App() {
 
@@ -43,8 +44,8 @@ function App() {
           <Route path='/user/editprofile' element={<EditProfile />} />
           <Route path='/checkout' element={<Checkout />} />
           <Route path='/checkoutsuccess' element={<CheckoutSuccess />} />
+          <Route path='/checkout/fail' element={<CheckoutFailure />} />
         </Route>
-        
 
         <Route path='*' element={<NotFound />}></Route>
       </Routes>
--- src/Pages/Payment/Checkout.jsx
+++ src/Pages/Payment/Checkout.jsx
@@ -12,7 +12,6 @@ function Checkout() {
     const navigate = useNavigate();
     const razorpayKey = useSelector((state) => state?.razorpay?.key);
     const subscription_id = useSelector((state) => state?.razorpay?.subscription_id);
-    const isPaymentVerified = useSelector((state) => state?.razorpay?.isPaymentVerified);
     const userData = useSelector((state) => state?.auth?.data);
 
     const paymentDetails = {
